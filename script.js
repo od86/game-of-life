72307d441d49d54f0d7d4f7dd5223e309b256d04
od86 <178620777+od86@users.noreply.github.com>
2026-03-31 10:09:10 +0100

    Made it so that when squares are clicked they swap to different color

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let board = document.querySelector('.board');
+const board = document.querySelector('.board');
+const playButton = document.querySelector('.play-btn')
+let allowedSquarePick = true;
 
 function create_board() {
   for (let x = 0; x < 50; x++) {
@@ -7,11 +9,25 @@ function create_board() {
     board.appendChild(row);
 
     for (let y = 0; y < 50; y++) {
-      square = document.createElement('div');
+      let square = document.createElement('div');
       square.classList.add('square');
+      square.id = `${x}/${y}`
       row.appendChild(square);
+
+      square.addEventListener('click', () => { selectSquare(square) });
     }
   }
 }
 
+function selectSquare(square) {
+  if (allowedSquarePick == false) { return; }
+
+  if (square.style.backgroundColor == "red") {
+    square.setAttribute("style", "background-color: rgb(105, 105, 105);")
+  } else {
+    square.setAttribute("style", "background-color: red;")
+  }
+}
+
 create_board();
+

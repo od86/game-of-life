d8bbd6a0e9636245b9556a2d2a7397dc6c57968e
od86 <178620777+od86@users.noreply.github.com>
2026-03-31 13:32:26 +0100

    Added multiple methods for game to js Added 0 to coords that are less than 10 Added check to see how many neighbors a square has Changed selectSquare to toggle class instead of add style

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const board = document.querySelector('.board');
 const playButton = document.querySelector('.play-btn')
+const stopButton = document.querySelector('.stop-btn');
+
 let allowedSquarePick = true;
+let gamePlaying = false;
 
 function create_board() {
   for (let x = 0; x < 50; x++) {
@@ -11,7 +14,7 @@ function create_board() {
     for (let y = 0; y < 50; y++) {
       let square = document.createElement('div');
       square.classList.add('square');
-      square.id = `${x}/${y}`
+      square.id = `x${fixCoord(x)}-y${fixCoord(y)}`
       row.appendChild(square);
 
       square.addEventListener('click', () => { selectSquare(square) });
@@ -19,15 +22,96 @@ function create_board() {
   }
 }
 
+function fixCoord(coord) { 
+  return coord < 10 ? `0${coord}` : coord
+}
+
 function selectSquare(square) {
   if (allowedSquarePick == false) { return; }
 
-  if (square.style.backgroundColor == "red") {
-    square.setAttribute("style", "background-color: rgb(105, 105, 105);")
-  } else {
-    square.setAttribute("style", "background-color: red;")
-  }
+  square.classList.toggle('alive')
 }
 
 create_board();
 
+
+
+playButton.addEventListener('click', () => {
+  let gameOfLifeLoop = setInterval(gameOfLife, 5000);
+  stopButton.addEventListener('click', () => {
+    setTimeout(() => { clearInterval(gameOfLifeLoop); }, 500); 
+  })
+});
+
+function gameOfLife() {
+  allowedSquarePick = false;
+
+  for (let x = 0; x < 50; x++) {
+    for (let y = 0; y < 50; y++) { 
+      aliveOrDead(document.querySelector(`#x${fixCoord(x)}-y${fixCoord(y)}`)); 
+    }
+  }
+}
+
+// x00-y00
+function aliveOrDead(square) {
+  let squareId = square.id.split('');
+  let squareState = square.classList[1] == "alive" ? true : false // Alive is true, dead is false
+  let neighbors = numOfNeighbors(squareId);
+}
+
+function numOfNeighbors(squareId) {
+  let xCoord = parseInt(`${squareId[1]}${squareId[2]}`);
+  let yCoord = parseInt(`${squareId[5]}${squareId[6]}`);
+  let aliveNeighbors = 0;
+
+    // Top ones
+  if (document.querySelector(`#x${fixCoord(xCoord + 1)}-y${fixCoord(yCoord + 1)}`) != null && 
+      document.querySelector(`#x${fixCoord(xCoord + 1)}-y${fixCoord(yCoord + 1)}`).classList[1] == "alive") {
+    aliveNeighbors += 1;
+  }
+
+  if (document.querySelector(`#x${fixCoord(xCoord + 1)}-y${fixCoord(yCoord - 1)}`) != null && 
+      document.querySelector(`#x${fixCoord(xCoord + 1)}-y${fixCoord(yCoord - 1)}`).classList[1] == "alive") {
+    aliveNeighbors += 1;
+  }
+
+  if (document.querySelector(`#x${fixCoord(xCoord + 1)}-y${fixCoord(yCoord)}`) != null && 
+      document.querySelector(`#x${fixCoord(xCoord + 1)}-y${fixCoord(yCoord)}`).classList[1] == "alive") {
+    aliveNeighbors += 1;
+  }
+
+  // Middle ones
+  if (document.querySelector(`#x${fixCoord(xCoord)}-y${fixCoord(yCoord + 1)}`) != null && 
+      document.querySelector(`#x${fixCoord(xCoord)}-y${fixCoord(yCoord + 1)}`).classList[1] == "alive") {
+    aliveNeighbors += 1;
+  }
+
+  if (document.querySelector(`#x${fixCoord(xCoord)}-y${fixCoord(yCoord - 1)}`) != null && 
+      document.querySelector(`#x${fixCoord(xCoord)}-y${fixCoord(yCoord - 1)}`).classList[1] == "alive") {
+    aliveNeighbors += 1;
+  }
+
+  // Bottom ones
+  if (document.querySelector(`#x${fixCoord(xCoord - 1)}-y${fixCoord(yCoord + 1)}`) != null && 
+      document.querySelector(`#x${fixCoord(xCoord - 1)}-y${fixCoord(yCoord + 1)}`).classList[1] == "alive") {
+    aliveNeighbors += 1;
+  }
+
+  if (document.querySelector(`#x${fixCoord(xCoord - 1)}-y${fixCoord(yCoord - 1)}`) != null && 
+      document.querySelector(`#x${fixCoord(xCoord - 1)}-y${fixCoord(yCoord - 1)}`).classList[1] == "alive") {
+    aliveNeighbors += 1;
+  }
+
+  if (document.querySelector(`#x${fixCoord(xCoord - 1)}-y${fixCoord(yCoord)}`) != null && 
+      document.querySelector(`#x${fixCoord(xCoord - 1)}-y${fixCoord(yCoord)}`).classList[1] == "alive") {
+    aliveNeighbors += 1;
+  }
+
+  return aliveNeighbors;
+}
+
+// Live cell: < 2 neighbors = dies
+// Live cell: 2 || 3 neighbors = survives
+// Live cell: > 3 neighbors = dies
+// Dead cell: == 3 neighbors = survives
